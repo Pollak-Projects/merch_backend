1afaf87d99dacf882180461aa46a3968952ab117
Feke András <feke13@gmail.com>
2024-10-06 14:52:11 +0200

    Add Prisma generate step to Dockerfile and recreate User table migration

--- prisma/migrations/20241006124654_init/migration.sql
+++ prisma/migrations/20241006124654_init/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE `User` (
     `password` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
