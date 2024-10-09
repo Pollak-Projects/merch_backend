b2a29000eaf16e51edafb0c2c6e9ab43d5c6d47a
Feke András <feke13@gmail.com>
2024-10-09 11:14:52 +0200

    Temporary models, routers

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,8 +1,31 @@
 import { Request, Response, NextFunction } from "express";
+import axios from  "axios";
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  // Authentication logic here
-  next();
+const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(" ")[1];
+
+      if (!token) {
+        return res.status(401).json({ message: "No token provided" });
+      }
+
+      const response = await axios.post(
+        "https://api.huszka.xyz/validate-token",
+        { token }
+      );
+
+      if (response.status === 200) {
+        next();
+      } else {
+        res.status(401).json({ message: "Unauthorized" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+
+
+
+
 };
 
 export default authMiddleware;
--- app/routes/login.ts
+++ app/routes/login.ts
@@ -1,21 +1,25 @@
 import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
+import authMiddleware from "../middleware/auth";
 const loginRouter = Router();
 const prisma = new PrismaClient();
 
-loginRouter.get("/login", async (req, res) => {
-  await prisma.user
-    .create({
-      data: {
-        email: "asd21",
-        password: "asd",
-        name: "fdsgdfgdfg",
-      },
-    })
-    .catch((err: Error) => {
-      console.log(err);
-    });
-  res.send("User logged in");
-});
+loginRouter.get(
+  "/login",
+  async (req, res) => {
+    await prisma.user
+      .create({
+        data: {
+          email: "asd21",
+          password: "asd",
+          name: "fdsgdfgdfg",
+        },
+      })
+      .catch((err: Error) => {
+        console.log(err);
+      });
+    res.send("User logged in");
+  },
+);
 
 export default loginRouter;
